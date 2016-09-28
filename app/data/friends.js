49f25cd34533c1cbbf529749029c27533f7dc482
ArcyFlor <arcylizet@ucla.edu>
2016-09-28 13:05:21 -0700

    updated matching functionality

---
+++ app/data/friends.js
@@ -0,0 +1,38 @@
+var friendsData = [
+{
+  name:"Biker Paul",
+  photo:"http://i.giphy.com/xTiTnfz3RILSYwD0UE.gif",
+  scores:[1,5,3,4,2,1,5,4,4,1]
+},
+{
+  name:"Mustache Paul",
+  photo:"http://i.giphy.com/Q378LBQHgGufm.gif",
+  scores:[5,1,4,4,5,1,2,5,4,1]
+},
+{
+  name:"Paul Jesus",
+  photo:"http://i.giphy.com/bBt4Oa8vknzUY.gif",
+  scores:[5,3,4,1,4,2,3,4,1,2]
+},
+{
+  name:"Papa Paul",
+  photo:"http://i.giphy.com/x3AJpjty3RdrW.gif",
+  scores:[4,4,3,2,5,3,5,1,2,4]
+},
+{
+  name:"Superhero Paul",
+  photo:"http://i.giphy.com/O1PEaUAQF728M.gif",
+  scores:[2,3,5,4,1,2,3,5,5,3]
+},
+{
+  name:"Slappin the Bass Paul",
+  photo:"http://i.giphy.com/xnpP4vot0nDSE.gif",
+  scores:[2,2,3,5,4,2,1,1,4,5]
+},
+{
+  name:"Surfer Paul",
+  photo:"http://i.giphy.com/mztEiyM7hzjDG.gif",
+  scores:[3,2,1,4,1,2,3,4,1,1]
+},
+];
+module.exports= friendsData;
--- server.js
+++ server.js
@@ -9,14 +9,15 @@ var path = require('path');
 var app = express();
 var PORT = 8000;
 
-require('./app/routing/api-routes.js')(app); 
-require('./app/routing/html-routes.js')(app);
 
 // Sets up the Express app to handle data parsing 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+require('./app/routing/api-routes.js')(app); 
+require('./app/routing/html-routes.js')(app);
 
 //sets up external routes
 app.use(express.static(path.join(__dirname, 'app', 'public')));
